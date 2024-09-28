71e8059fa1542216487d2753432fc23cf1b37a53
Camile Eduarda <165083973+CamileECF@users.noreply.github.com>
2024-09-28 14:27:33 -0300

    Update Atividade_3-2.c

--- Atividade_3-2.c
+++ Atividade_3-2.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <limits.h>
+#include <locale.h>
 
 int main() {
-
-
-    /* TABELA DE VARI¡VEIS DO MÕNIMO AO M¡XIMO */
-    
+    setlocale(LC_ALL,"portuguese");
     printf("| TIPO                   | MINIMO                | MAXIMO              |\n");
     printf("|________________________|_______________________|_____________________|\n");
     printf("| char                   | %d                  | %d                 |\n", CHAR_MIN, CHAR_MAX);
@@ -16,17 +14,15 @@ int main() {
     printf("| unsigned long int      | 0                     | %lu          |\n", ULONG_MAX);
     printf("| long long int          | %lld  | %lld |\n", LLONG_MIN, LLONG_MAX);
     printf("| unsigned long long int | 0                     | %llu|\n", ULLONG_MAX);
-
-    /* Exemplo de valor fora do limite */
     
     int num = INT_MAX; 
     printf("\nExemplo de valor fora do limite \n");
     
-    printf("Valor original de num: %d\n", num);
+    printf("Valor original: %d\n", num);
     
-num += 1;            /* Adicionando 1 ao valor m·ximo, logo, ultrapassando o limite suportado pela vari·vel */  
+num += 1;            /* Adicionando 1 ao valor m√°ximo, logo, ultrapassando o limite suportado pela vari√°vel */  
 
-    printf("O valor apresentado apos passar do limite eh de: %d\n", num);   
+    printf("O valor apresentado ap√≥s passar do limite √© de: %d\n", num);   
 
     return 0;
 }
